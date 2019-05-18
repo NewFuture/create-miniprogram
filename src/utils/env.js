4eab8b23a40a6237b1f5dc23c8f062e881d4c259
NewFuture <github@newfuture.cc>
2019-05-19 00:53:03 +0800

    style(eslint): auto fix

--- src/utils/env.js
+++ src/utils/env.js
@@ -14,18 +14,18 @@ function isWindows() {
  */
 function install(cwd) {
   return new Promise((resolve, reject) => {
-    const suffix = isWindows() ? '.cmd' : '';
+    const suffix = isWindows() ? '.cmd' : ''
     const cmd = (isYarn() ? 'yarn' : 'npm') + suffix
-    const ps = require('child_process').spawn(cmd, ['install'], { cwd, stdio: 'inherit' });
+    const ps = require('child_process').spawn(cmd, ['install'], {cwd, stdio: 'inherit'})
     ps.on('close', (code) => {
       if (code !== 0) {
         reject(code)
-        console.error(`依赖自动安装出错${code},可手动安装!`);
+        console.error(`依赖自动安装出错${code},可手动安装!`)
       } else {
-        console.log(`依赖安装完成!`);
+        console.log('依赖安装完成!')
         resolve()
       }
-    });
+    })
   })
 }
 
